24b2bbae77ae7c6c0d1e15f17320791c085cb0b5
כפיר בגדדי <BGkfir@gmail.com>
2023-01-28 11:33:43 +0200

    finish project

--- app/src/main/java/com/example/final_project/fragments/FragmentHome.java
+++ app/src/main/java/com/example/final_project/fragments/FragmentHome.java
@@ -4,15 +4,10 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.final_project.R;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,17 +15,14 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.example.final_project.adapter.StartAdapter;
-import com.example.final_project.adapter.myAdapter;
 import com.example.final_project.models.dataModel;
 import com.example.final_project.recyclerViewInterface;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.mlkit.common.model.DownloadConditions;
 import com.google.mlkit.nl.translate.TranslateLanguage;
 import com.google.mlkit.nl.translate.Translation;
 import com.google.mlkit.nl.translate.Translator;
@@ -39,23 +31,15 @@ import com.google.mlkit.nl.translate.TranslatorOptions;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
-import java.util.TimerTask;
 
 
 public class FragmentHome extends Fragment implements recyclerViewInterface{
 
-    private RecyclerView recyclerView;
-    private myAdapter adapter;
     private DatabaseReference databaseref;
     private List<dataModel> dataSet;
     private Button  multipleLanguagesBtn;
-    private ProgressBar progressBar;
-    private Timer timer;
-    private int count = 0;
     Translator englishHebrewTranslator;
     private StartAdapter startAdapter;
-
-    Translator Translator;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,27 +51,83 @@ public class FragmentHome extends Fragment implements recyclerViewInterface{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_home, container, false);
-
         dataSet = new ArrayList<>();
-        databaseref = FirebaseDatabase.getInstance().getReference("animals");
-        databaseref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                dataSet.clear();
-                for (DataSnapshot postSnapeshot : snapshot.getChildren()){
-                    dataModel dataMod = postSnapeshot.getValue(dataModel.class);
 
-                    dataSet.add(dataMod);
+
+            databaseref = FirebaseDatabase.getInstance().getReference("animals");
+            databaseref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    dataSet.clear();
+                    for (DataSnapshot postSnapeshot : snapshot.getChildren()){
+                        dataModel dataMod = postSnapeshot.getValue(dataModel.class);
+
+                        dataSet.add(dataMod);
+                    }
+                    startAdapter=new StartAdapter();
+                    startAdapter.startAdapterOnCall(view,dataSet,FragmentHome.this);
+
                 }
-                startAdapter=new StartAdapter();
-                startAdapter.startAdapterOnCall(view,dataSet,FragmentHome.this);
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
 
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                }
+            });
 
-            }
-        });
+
+            databaseref = FirebaseDatabase.getInstance().getReference("animals");
+            databaseref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    dataSet.clear();
+                    for (DataSnapshot postSnapeshot : snapshot.getChildren()){
+                        dataModel dataMod = postSnapeshot.getValue(dataModel.class);
+                        dataSet.add(dataMod);
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+            TranslatorOptions options =
+                    new TranslatorOptions.Builder()
+                            .setSourceLanguage(TranslateLanguage.ENGLISH)
+                            .setTargetLanguage(TranslateLanguage.HEBREW)
+                            .build();
+            englishHebrewTranslator = Translation.getClient(options);
+
+            englishHebrewTranslator.downloadModelIfNeeded().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+
+                    for (int i=0; i<dataSet.size();i++ ){
+                        int finalI = i;
+
+                        englishHebrewTranslator.translate(dataSet.get(i).getName().toString()).addOnSuccessListener(new OnSuccessListener<String>() {
+                            @Override
+                            public void onSuccess(String s) {
+//                                startAdapter=new StartAdapter();
+                                dataSet.get(finalI).setName(s.toString());
+
+                            }
+                        });
+                        englishHebrewTranslator.translate(dataSet.get(i).getDataAnimal().toString()).addOnSuccessListener(new OnSuccessListener<String>() {
+                            @Override
+                            public void onSuccess(String s) {
+                                dataSet.get(finalI).setDataAnimal(s.toString());
+
+
+                            }
+                        });
+
+                    }
+                    startAdapter=new StartAdapter();
+                    startAdapter.startAdapterOnCall(view,dataSet,FragmentHome.this);
+                }
+            });
+
 
         Button createButton = view.findViewById(R.id.createButton);
         createButton.setOnClickListener(new View.OnClickListener() {
@@ -100,8 +140,8 @@ public class FragmentHome extends Fragment implements recyclerViewInterface{
 
         multipleLanguagesBtn = view.findViewById(R.id.multipleLanguagesButton);
         multipleLanguagesBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
 
+            @Override
             public void onClick(View v) {
                 TranslatorOptions options =
                         new TranslatorOptions.Builder()
@@ -120,7 +160,6 @@ public class FragmentHome extends Fragment implements recyclerViewInterface{
                             englishHebrewTranslator.translate(dataSet.get(i).getName().toString()).addOnSuccessListener(new OnSuccessListener<String>() {
                                 @Override
                                 public void onSuccess(String s) {
-                                    startAdapter=new StartAdapter();
                                     dataSet.get(finalI).setName(s.toString());
 
                                 }
@@ -128,28 +167,18 @@ public class FragmentHome extends Fragment implements recyclerViewInterface{
                             englishHebrewTranslator.translate(dataSet.get(i).getDataAnimal().toString()).addOnSuccessListener(new OnSuccessListener<String>() {
                                 @Override
                                 public void onSuccess(String s) {
-                                    startAdapter=new StartAdapter();
                                     dataSet.get(finalI).setDataAnimal(s.toString());
-                                    startAdapter.startAdapterOnCall(view,dataSet,FragmentHome.this);
-
                                 }
                             });
-
                         }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-
+                        startAdapter=new StartAdapter();
+                        startAdapter.startAdapterOnCall(view,dataSet,FragmentHome.this);
                     }
                 });
             }
         });
 
         return  view;
-    }
-    void  fun(){
-
     }
 
 
